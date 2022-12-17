1a734f06080f0e2a440bd2635563f86f69694e15
moshix <moshix@gmail.com>
2022-12-17 06:58:25 -0600

    re-instate deleted lines

--- install.bash
+++ install.bash
@@ -15,9 +15,77 @@
 # may change it here.
 
 source ./Version
-logextension
-cpwd=$(pwd)
-curdir=$(basename "$cpwd")
+
+SUDO="sudo"
+export SUDO
+
+set_colors() {
+    red=`tput setaf 1`
+    green=`tput setaf 2`
+    yellow=`tput setaf 3`
+    blue=`tput setaf 4`
+    magenta=`tput setaf 5`
+    cyan=`tput setaf 6`
+    white=`tput setaf 7`
+    bold=`tput bold`
+    uline=`tput smul`
+    blink=`tput blink`
+    rev=`tput rev`
+    reset=`tput sgr0`
+}
+
+test_sudo () {
+#    echo "${yellow}Testing if '$SUDO' command works ${reset}"
+    if [[ $($SUDO id -u) -ne 0 ]]; then
+        echo "${rev}${red}🤔 $SUDO did not set us to uid 0; you must run this script with a user that has $SUDO privileges.${reset}"
+        exit 1
+    fi
+}
+
+check_if_root () {
+    # check if I am root and terminate if so
+    if [[ $(id -u) -eq 0 ]]; then
+        echo "${rev}${red}🤔 You are root. You must run this installer as a regular user. Terminating...${reset}"
+        exit 1
+    fi
+}
+
+logextension=`date "+%F-%T"`
+logit () {
+    # log to file all messages
+    logdate=`date "+%F-%T"`
+    echo "$logdate:$1" >> ./logs/AccessAudit_installer.log."$logextension"
+}
+
+
+check_os () {
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        echo
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "${rev}${red}😬 MacOS detected. Sorry, MacOS is not yet supported.${reset}"
+        exit 1
+    elif [[ "$OSTYPE" == "cygwin" ]]; then
+        echo "${rev}${red}😬 Cygwin detected. Sorry, Cygwin is not supported.${reset}"
+        exit 1
+    elif [[ "$OSTYPE" == "win32" ]]; then
+        echo "${rev}${red}😬 Windows detected. Sorry, Windows is not supported.${reset}"
+        exit 1
+    else
+        echo "${rev}${red}😬 Unrecognized operating system. Exiting now.${reset}"
+        exit 1
+    fi
+    os=`awk -F= '/^NAME/{print $2}' /etc/os-release` # OS type is in $os !!
+
+}
+
+
+#main here
+set_colors # to get terminal coloring settings
+
+# are we running from the AccessAudit directory (instead of higher or lower directory?)
+cpwd=`pwd`
+curdir=`basename "$cpwd"`
+
 
 if [[ "$curdir"  != "AccessAudit" ]]; then
 	echo "${rev}${red}😬 This script needs to be executed from inside the AccessAudit directory. Please retry. ${red}"
